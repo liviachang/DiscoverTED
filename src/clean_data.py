e4eb3e11181f892c8b56ef7deddc77a4135cd255
liviachang <liviachang@gmail.com>
2016-11-11 22:38:13 -0800

    add comments to clean_data.py

--- src/clean_data.py
+++ src/clean_data.py
@@ -5,15 +5,20 @@ import unicodedata
 import sys
 
 def clean_idiap_talk_titles(titles_orig): # x=idf.title
+  ## flatten the series of talk titles
   titles = []
   for t in titles_orig: # type(t)=list
     titles = titles + t
 
+  ## encode and clean data so it maps better with user data
   for tidx, tdata in enumerate(titles):
     titles[tidx] = tdata.encode('utf-8', 'ignore').rstrip()
   return titles
 
 def clean_idiap_user_fav_titles(titles_orig): # x = udf.favorites
+  ## encode and clean data so it maps better with talk data
+  ## it is time-consuming to apply the cleaning
+  ## so cleaning is applied to unique titles first then all titles
   tunique = titles_orig.unique()
   tcleaned = []
   for t in tunique:
@@ -26,58 +31,84 @@ def clean_idiap_user_fav_titles(titles_orig): # x = udf.favorites
 
 def merge_talk_data():
   print 'Merging talk data from idiap and my scraping results'
-  idiap_tfn = '/Users/liviachang/TED/idiap/ted_talks-10-Sep-2012.json'
-  my_tfn = '/Users/liviachang/Galvanize/capstone/data/talks_info_scraped.csv'
 
+  ## load and clean talk data from idiap
+  idiap_tfn = '/Users/liviachang/TED/idiap/ted_talks-10-Sep-2012.json'
   idf = pd.read_json(idiap_tfn)
   idf['title_idiap'] = clean_idiap_talk_titles(idf.title)
   idf['url'] = [x.replace('.html', '') for x in idf['url']]
 
+  ## load data created by src/scrape_ted_talks.py
+  my_tfn = '/Users/liviachang/Galvanize/capstone/data/talks_info_scraped.csv'
   mdf = pd.read_csv(my_tfn)
 
+  ## use url to map idiap and my scraped talk data
   idiap_cols = ['title_idiap', 'url', 'related_themes', 'related_videos']
   tdf = pd.merge(mdf, idf[idiap_cols], how='left', on=['url'])
   tdf.ix[tdf.title_idiap.isnull(), 'title_idiap'] = ''
 
+  ## show basic stats to check the merge quality
   print 'idf.shape = {}, mdf.shape = {}, tdf.shape = {}'.format(\
     idf.shape, mdf.shape, tdf.shape)
   
+  ## export the data for future use
   tdf.to_csv('/Users/liviachang/Galvanize/capstone/data/talks_info_merged.csv', index=False)
+
   return tdf
   
-def flatten_user_favorites(x):
+def flatten_user_favorites(x): ## x = udf.favorites[0]
+  ## from (user, [n titles]) to a list with n tuples
+  ## each tuple is (user_id, favorite_title)
   favs = [f for f in x['favorites']]
   uids = [x['user_id']] * len(favs)
   user_fav_pairs = zip(uids, favs)
   return user_fav_pairs
 
 def clean_user_data():
+  ## load idiap user data
   ufn = '/Users/liviachang/TED/idiap/ted_users-10-Sep-2012.json'
   udf_orig = pd.read_json(ufn)
+
+  ## flatten user data 
+  ## each row for each "user/favorite title" combination
   udf = udf_orig.apply(flatten_user_favorites, axis=1)
   udf = list(itertools.chain(*udf))
   udf = pd.DataFrame(udf, columns=['uid_idiap', 'fav_title'])
+
+  ## add fid as "favorite_id" 
+  ## so it is easier to identify the record for debugging
   udf['fid'] = range(udf.shape[0])
+
+  ## clean fav_title data
+  ## so it is easier to map with titles in talk data
   udf['fav_title'] = clean_idiap_user_fav_titles(udf['fav_title'])
+
   return udf
 
 def transform_user_data():
   print '\nTransforming user data from merged talk data and idiap user data'
 
+  ## load talk data
   tfn = '/Users/liviachang/Galvanize/capstone/data/talks_info_merged.csv'
   tdf = pd.read_csv(tfn)
 
+  ## load user data
   udf = clean_user_data()
 
-  ## generate rating data
+  ## generate rating data from talk + user data
+  ## each row for each "user/favorite title" combination
   tcols = ['title_idiap', 'tid']
   rdf = pd.merge(udf, tdf[tcols], how='left', left_on='fav_title', right_on='title_idiap')
 
+  ## print basic stats to check the merge quality
   print rdf.info()
 
+  ## some favorite talks in the user data is no longer available on ted.com
+  ## remove them (~0.05% of the original user data)
   rdf = rdf.dropna()
   print 'rdf.shape = {}'.format(rdf.shape)
 
+  ## export the data for model building
   rdf.to_csv('/Users/liviachang/Galvanize/capstone/data/users_info_transformed.csv', index=False)
   return rdf
 
